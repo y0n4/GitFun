389c72d2f772c9d7905c741ece3c4aa574e915ed
yona <04l.yona@gmail.com>
2018-03-25 02:17:25 -0400

    longestCommonPrefix problem finally solved 4 fun

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -32,10 +32,25 @@ bar
 ""
 bar
 MATCH! continue with next string (prefix is completely gone...)
+*/
+
 const longestCommonPrefix = arrayOfStrings => {
   // initialize the "longest prefix" with the entire first string
+  var longest = arrayOfStrings[0];
   // iterate from the 2nd string onward
-    // shorten the prefix from the end until there's a match (which is guaranteed when the string
-        is empty)
- };
-*/
+  for(var i = 1; i < arrayOfStrings.length; i++){
+    // shorten the prefix from the end until there's a match (which is guaranteed when the string is empty)
+    // when 1st element is not found in string
+    if(!arrayOfStrings.indexOf(longest)){
+      //then remove last character and then search again
+      longest = longest.slice(0, longest.length - 1);
+    }
+
+    // searching through and when 1st element is finally found in string
+    // OR 1st element reaches to an empty string
+    if(arrayOfStrings.indexOf(longest) || longest === ''){
+      // match is found
+      return 'MATCH!';
+    }
+  }
+};
